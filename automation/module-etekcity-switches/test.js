99b1eca955216694f477d328d2fb23d5a2b1a646
Christopher <cn@wtf-solutions.de>
2016-10-22 17:03:14 +0200

    implemented first prototype script

---
+++ automation/module-etekcity-switches/test.js
@@ -0,0 +1,35 @@
+var args = process.argv, //running arguments from command line
+    rpi433    = require('rpi-433'),
+    rfEmitter = rpi433.emitter({pin: 0, pulseLength: 180}),
+    switchGroups = require('./switch.groups.json')
+    ;
+
+function switchMe(group, power) {
+
+  if(power === 'on' || power === 'off') {
+    var switchGroup = switchGroups[group],
+        signal = undefined;
+        ;
+
+    if(power === 'on') {
+      signal = switchGroup.on;
+    } else {
+      signal = switchGroup.off;
+    }
+
+    rfEmitter.sendCode(signal, function(error, stdout) {
+      if(error) {
+        console.log('Code was not sent, reason: ', error);
+      } else {
+        console.log('Code sent: ', stdout);
+      };
+    });
+  } else {
+      console.log('No valid command. [group-id] [on/off]');
+  }
+
+
+
+}
+
+switchMe(args[2], args[3]);
