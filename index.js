3406c15d563a27cb7989c158d6454fcfde3d678f
Christopher <cn@wtf-solutions.de>
2016-10-22 15:51:58 +0200

    added dependencies and further project setup
    added wiring-pi and rpi-433 to dependencies.

---
+++ index.js
@@ -0,0 +1,5 @@
+/*
+Entry point is the javascript file that will be invoked when consumers of your
+module “require” it, this file will include the main logic for your module,
+or if it is a large module you can export public functions found with other files
+*/
