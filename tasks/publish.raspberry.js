db304d16e5e37a72d4c109b12fd76656ffe57d28
Christopher <cn@wtf-solutions.de>
2016-10-22 14:54:56 +0200

    enhancements

--- tasks/publish.raspberry.js
+++ tasks/publish.raspberry.js
@@ -2,7 +2,7 @@ let ncp = require('ncp').ncp;
 
 ncp.limit = 16;
 
-let source = '../../technical-prototype/';
+let source = '/Users/cneuwirth/Office/Dev/smarthome/technical-prototype';
 let destination = '/Volumes/Home\ Directory/Dev/smarthome/technical-prototype';
 
 ncp(source, destination, function (err) {
