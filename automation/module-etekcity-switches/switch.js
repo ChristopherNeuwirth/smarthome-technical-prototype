95d53e5a6bd0f38e4a524b222c8ce4f050c2cf34
Christopher <cn@wtf-solutions.de>
2016-10-23 17:50:39 +0200

    implemented api server and route

---
+++ automation/module-etekcity-switches/switch.js
@@ -0,0 +1,33 @@
+let args = process.argv, //running arguments from command line
+    rpi433    = require('rpi-433'),
+    rfEmitter = rpi433.emitter({pin: 0, pulseLength: 180}),
+    switchGroups = require('./switch.groups.json')
+    ;
+
+let switchMe = (group, power) => {
+  let response;
+  if(power === 'on' || power === 'off') {
+    var switchGroup = switchGroups[group],
+        signal = undefined;
+        ;
+
+    if(power === 'on') {
+      signal = switchGroup.on;
+    } else {
+      signal = switchGroup.off;
+    }
+
+    rfEmitter.sendCode(signal, function(error, stdout) {
+      if(error) {
+        response = 'Code was not sent, reason: ' + error;
+      } else {
+        response = 'Code sent: ' + stdout;
+      };
+    });
+  } else {
+      response = 'No valid command';
+  }
+  return response;
+}
+
+module.exports.switchMe = switchMe;
--- tasks/publish.raspberry.js
+++ tasks/publish.raspberry.js
@@ -4,8 +4,11 @@ ncp.limit = 16;
 
 let source = '/Users/cneuwirth/Office/Dev/smarthome/technical-prototype';
 let destination = '/Volumes/Home\ Directory/Dev/smarthome/technical-prototype';
+let options = {
+  filter: '^(?!.*node_modules).*$'
+}
 
-ncp(source, destination, function (err) {
+ncp(source, destination, options, function (err) {
   if (err) {
     return console.error(err);
   }
